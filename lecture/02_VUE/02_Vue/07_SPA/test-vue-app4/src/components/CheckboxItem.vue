1e0701b506e4886b88ee8493f51c91b7d25603ce
tuchongkim <2019102076@khu.ac.kr>
2026-03-25 16:15:18 +0900

    day15: 단일 파일 컴포넌트를 이용한 vue 애플리케이션 이론 학습

---
+++ lecture/02_VUE/02_Vue/07_SPA/test-vue-app4/src/components/CheckboxItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <li><input type="checkbox" :checked="checked" />{{ name }}</li>
+</template>
+
+<script>
+export default {
+  name: 'CheckboxItem',
+  props: ['name', 'checked'],
+  //   data() {
+  //     return {
+  //       checked: false,
+  //     };
+  //   },
+};
+</script>
