3b27ac8c1b471577c261fc728919ca45b6c76528
tuchongkim <2019102076@khu.ac.kr>
2026-03-30 23:58:00 +0900

    day19: vue 단일 파일 컴포넌트 실습(심화) - todoList 예제 리팩토링

---
+++ practice/02_VUE/02_Vue/ch08_single_file_component/todolist-app/src/components/InputTodo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <div class="input-group">
+        <input
+          type="text"
+          class="form-control"
+          name="msg"
+          placeholder="할일을 여기에 입력!"
+          v-model.trim="newTodo"
+          @keyup.enter="addTodoHandler"
+        />
+        <button class="btn btn-primary" @click="addTodoHandler">추가</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputTodo',
+  data() {
+    return {
+      newTodo: '',
+    };
+  },
+  methods: {
+    addTodoHandler() {
+      if (this.newTodo.length >= 3) {
+        this.$emit('add-todo', this.newTodo);
+        this.newTodo = '';
+      }
+    },
+  },
+};
+</script>
